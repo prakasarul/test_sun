8440199280a1e178ebd63f9a78e46b250578f252
gopac25 <39807096+gopac25@users.noreply.github.com>
2019-05-26 01:20:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-  access_key = "ACCESS_KEY_HERE"
-  secret_key = "SECRET_KEY_HERE"
+  access_key = "$AWS_ACCESS_KEY_ID"
+  secret_key = "$AWS_SECRET_ACCESS_KEY"
   region     = "us-east-1"
 }
 

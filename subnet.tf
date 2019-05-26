b446b75fe082fd71824b564c49b48a9274d78600
gopac25 <39807096+gopac25@users.noreply.github.com>
2019-05-26 21:37:45 +0530

    Update subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -10,7 +10,7 @@ resource "aws_subnet" "moonshot-public" {
     }
 }
 resource "aws_subnet" "moonshot-private-web" {
-    vpc_id = "${aws_vpc.moonshot-public.id}"
+    vpc_id = "${aws_vpc.moonshot.id}"
     cidr_block = "10.0.2.0/24"
     map_public_ip_on_launch = "false"
     availability_zone = "ap-south-1a"
@@ -20,7 +20,7 @@ resource "aws_subnet" "moonshot-private-web" {
     }
 }
 resource "aws_subnet" "moonshot-private-slave" {
-    vpc_id = "${aws_vpc.moonshot-public.id}"
+    vpc_id = "${aws_vpc.moonshot.id}"
     cidr_block = "10.0.3.0/24"
     map_public_ip_on_launch = "false"
     availability_zone = "ap-south-1b"

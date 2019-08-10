df756de08490473ce5c10ff8feae3006d20d9ce2
gopac25 <39807096+gopac25@users.noreply.github.com>
2019-08-10 21:11:58 +0200

    Update elb.tf

--- elb.tf
+++ elb.tf
@@ -1,35 +1,25 @@
-## Security Group for ELB
-resource "aws_security_group" "moonshot-elb" {
-  name = "moonshot-elb"
-  egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+resource "aws_elb" "my-elb" {
+  name = "my-elb"
+  subnets = ["${aws_subnet.moonshot-private-web.id}", "${aws_subnet.moonshot-private-app.id}"]
+  security_groups = ["${aws_security_group.elb-securitygroup.id}"]
+ listener {
+    instance_port = 80
+    instance_protocol = "http"
+    lb_port = 80
+    lb_protocol = "http"
   }
-  ingress {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-### Creating ELB
-resource "aws_elb" "moon-elb" {
-  name = "moon-elb"
-  security_groups = ["${aws_security_group.moonshot-elb.id}"]
-  availability_zones = ["${data.aws_availability_zones.all.names}"]
   health_check {
     healthy_threshold = 2
     unhealthy_threshold = 2
     timeout = 3
+    target = "HTTP:80/"
     interval = 30
-    target = "HTTP:8080/"
   }
-  listener {
-    lb_port = 80
-    lb_protocol = "http"
-    instance_port = "8080"
-    instance_protocol = "http"
+
+  cross_zone_load_balancing = true
+  connection_draining = true
+  connection_draining_timeout = 400
+  tags {
+    Name = "my-elb"
   }
 }

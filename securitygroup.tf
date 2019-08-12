86859d88f7bf425d397aa89faf44cdecf11f9854
gopac25 <39807096+gopac25@users.noreply.github.com>
2019-08-12 16:26:57 +0530

    Update securitygroup.tf

--- securitygroup.tf
+++ securitygroup.tf
@@ -20,7 +20,7 @@ resource "aws_security_group" "allow-ssh" {
     from_port = 2049
     to_port   = 2049
     protocol  = "tcp"
-    security_groups = ["${aws_security_group.allow-ssh.id}"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
   
   ingress {

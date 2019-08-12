a8d392ed63f69e57d861b7670f2bb141f29ad7dd
gopac25 <39807096+gopac25@users.noreply.github.com>
2019-08-12 15:39:07 +0530

    Update efs.tf

--- efs.tf
+++ efs.tf
@@ -1,27 +1,25 @@
-resource "aws_security_group" "efs_sg" {
-  name        = "moon_efs_sg"
-  vpc_id      = "${aws_vpc.moonshot.id}"
-
-  ingress {
-    from_port = 2049
-    to_port   = 2049
-    protocol  = "tcp"
-    security_groups = ["${aws_security_group.allow-ssh.id}"]
-  }
-
+resource "aws_efs_file_system" "webdata_efs" {
   tags {
-    Project         = "Moonshot"
+    Name            = "Web_Data_EFS"
   }
 }
 
-resource "aws_efs_file_system" "data_efs" {
-  tags {
-    Name            = "Data_EFS"
-  }
-}
-
-resource "aws_efs_mount_target" "data_efs_mount" {
+resource "aws_efs_mount_target" "webdata_efs_mount" {
   count                   = "3"
-  file_system_id          = "${aws_efs_file_system.data_efs.id}"
-  security_groups         = ["${aws_security_group.efs_sg.id}"]
+  file_system_id          = "${aws_efs_file_system.webdata_efs.id}"
+  subnet_id               = "${aws_subnet.moonshot-private-web.id}"
+  security_groups         = ["${aws_security_group.allow_ssh.id}"]
+}
+
+resource "aws_efs_file_system" "appdata_efs" {
+  tags {
+    Name            = "App_Data_EFS"
+  }
+}
+
+resource "aws_efs_mount_target" "appdata_efs_mount" {
+  count                   = "3"
+  file_system_id          = "${aws_efs_file_system.webdata_efs.id}"
+  subnet_id               = "${aws_subnet.moonshot-private-app.id}"
+  security_groups         = ["${aws_security_group.allow_ssh.id}"]
 }

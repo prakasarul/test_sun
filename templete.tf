c21654579127e0ecbaa06a771b43d512862d907e
gopac25 <39807096+gopac25@users.noreply.github.com>
2019-08-14 13:37:23 +0530

    Update templete.tf

--- templete.tf
+++ templete.tf
@@ -1,5 +1,12 @@
-resource "template_file" "user_data" {
-  template = "efs_mount.tpl"
+resource "template_file" "user_data_web" {
+  template = "efs_web_mount.tpl"
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "template_file" "user_data_app" {
+  template = "efs_app_mount.tpl"
   lifecycle {
     create_before_destroy = true
   }

08f424d15ee413b537e6fa169d84e053c08fe144
gopac25 <39807096+gopac25@users.noreply.github.com>
2019-08-14 13:33:54 +0530

    Update autoscaling.tf

--- autoscaling.tf
+++ autoscaling.tf
@@ -5,6 +5,7 @@ resource "aws_launch_configuration" "moonshot-launchconfig-web" {
   count                = "${var.count}"
   key_name             = "${aws_key_pair.mykey.key_name}"
   security_groups      = ["${aws_security_group.allow-ssh.id}"]
+  user_data = "${template_file.user_data.rendered}"
 }
 
 resource "aws_launch_configuration" "moonshot-launchconfig-app" {
@@ -14,6 +15,7 @@ resource "aws_launch_configuration" "moonshot-launchconfig-app" {
   count                = "${var.count}"
   key_name             = "${aws_key_pair.mykey.key_name}"
   security_groups      = ["${aws_security_group.allow-ssh.id}"]
+  user_data = "${template_file.user_data.rendered}"
 }
 
 resource "aws_autoscaling_group" "moonshot-autoscaling-web" {

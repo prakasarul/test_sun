b8718f4a06653218961a9d7535d25229a4250d0b
gopac25 <39807096+gopac25@users.noreply.github.com>
2019-08-18 21:23:24 +0200

    Update efs.tf

--- efs.tf
+++ efs.tf
@@ -1,5 +1,4 @@
 resource "aws_efs_file_system" "webdata_efs" {
-   vpc_id = "${aws_vpc.moonshot.id}"
    creation_token = "webdata_efs"
    performance_mode = "generalPurpose"
    throughput_mode = "bursting"
@@ -10,14 +9,12 @@ resource "aws_efs_file_system" "webdata_efs" {
  }
  
  resource "aws_efs_mount_target" "webdata_efs_1" {
-   vpc_id = "${aws_vpc.moonshot.id}"
    file_system_id  = "${aws_efs_file_system.webdata_efs.id}"
    subnet_id = "${aws_subnet.moonshot-private-web.id}"
    security_groups = ["${aws_security_group.allow-ssh.id}"]
  }
  
 resource "aws_efs_file_system" "appdata_efs" {
-   vpc_id = "${aws_vpc.moonshot.id}"
    creation_token = "appdata_efs"
    performance_mode = "generalPurpose"
    throughput_mode = "bursting"
@@ -28,7 +25,6 @@ resource "aws_efs_file_system" "appdata_efs" {
  }
  
  resource "aws_efs_mount_target" "appdata_efs_1" {
-   vpc_id = "${aws_vpc.moonshot.id}"
    file_system_id  = "${aws_efs_file_system.appdata_efs.id}"
    subnet_id = "${aws_subnet.moonshot-private-app.id}"
    security_groups = ["${aws_security_group.allow-ssh.id}"]

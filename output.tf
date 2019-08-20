84dde1fca1ab210c8ac86280bf74d78e0e917090
gopac25 <39807096+gopac25@users.noreply.github.com>
2019-08-20 15:10:10 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -21,22 +21,22 @@ output "efs_mount_target_ip_address" {
   value = "${aws_efs_mount_target.webdata_efs_1.*.ip_address}"
 }
 
-output "efs_filesystem_id" {
+output "efs_filesystem_id_1" {
   value = "${aws_efs_file_system.appdata_efs.id}"
 }
 
-output "efs_mount_target_id" {
+output "efs_mount_target_id_1" {
   value = "${aws_efs_mount_target.appdata_efs_1.*.id}"
 }
 
-output "efs_mount_target_dns_name" {
+output "efs_mount_target_dns_name_1" {
   value = "${aws_efs_mount_target.appdata_efs_1.*.dns_name}"
 }
 
-output "efs_mount_target_network_interface_id" {
+output "efs_mount_target_network_interface_id_1" {
   value = "${aws_efs_mount_target.appdata_efs_1.*.network_interface_id}"
 }
 
-output "efs_mount_target_ip_address" {
+output "efs_mount_target_ip_address_1" {
   value = "${aws_efs_mount_target.appdata_efs_1.*.ip_address}"
 }

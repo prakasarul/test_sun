0847b8b5ce10334ad0cc3643651c09271754dd3f
gopac25 <39807096+gopac25@users.noreply.github.com>
2019-08-20 15:15:25 +0530

    Update appmount.sh

--- appmount.sh
+++ appmount.sh
@@ -3,4 +3,4 @@
 sudo yum install -y nfs-utils
 sudo mkdir /efs
 sudo chmod 777 /efs
-sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 ${aws_efs_mount_target.appdata_efs_1.ip_address}:/ /efs
+sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 ${aws_efs_mount_target.appdata_efs_1.*.ip_address}:/ /efs

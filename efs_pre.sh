27ec0c9cee0e53cd1c3bf3a63f5561b3e03ff794
gopac25 <39807096+gopac25@users.noreply.github.com>
2019-08-21 20:32:09 +0530

    Create efs_pre.sh

---
+++ efs_pre.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+web=`terraform output|grep -w -A1 efs_mount_target_ip_address|tail -n 1|sed 's/ //g'`
+app=`terraform output|grep -w -A1 efs_mount_target_ip_address_1|tail -n 1|sed 's/ //g'`
+sed 's/retrans=2.*:/retrans=2 '$web':/g' webmount.sh
+sed 's/retrans=2.*:/retrans=2 '$app':/g' appmount.sh

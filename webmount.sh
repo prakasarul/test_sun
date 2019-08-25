fe6b49c7d2717e4da5a7478fe1a06381491678ee
gopac25 <39807096+gopac25@users.noreply.github.com>
2019-08-25 20:34:34 +0530

    Update webmount.sh

--- webmount.sh
+++ webmount.sh
@@ -4,4 +4,4 @@ sudo yum update -y
 sudo yum install -y nfs-utils
 sudo mkdir /efs
 sudo chmod 777 /efs
-# sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 "${aws_efs_mount_target.webdata_efs_1.*.ip_address}":/ /efs
+sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 "${aws_efs_mount_target.webdata_efs_1.*.ip_address}":/ /efs

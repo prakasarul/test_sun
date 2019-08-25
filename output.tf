07ecc1583c70144c33348c5e5f5fd5ddb9d457a4
gopac25 <39807096+gopac25@users.noreply.github.com>
2019-08-25 23:20:47 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -42,3 +42,11 @@ output "efs_mount_target_network_interface_id_1" {
 output "efs_mount_target_ip_address_1" {
   value = "${aws_efs_mount_target.appdata_efs_1.*.ip_address}"
 }
+
+vars {
+    web = "${aws_efs_mount_target.webdata_efs.*.ip_address}"
+  }
+
+vars {
+    app = "${aws_efs_mount_target.appdata_efs.*.ip_address}"
+  }

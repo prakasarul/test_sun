74fc9f5c9fb1b0aaae5f0513e9af5e2eb82f7222
gopac25 <39807096+gopac25@users.noreply.github.com>
2019-08-26 01:01:45 +0530

    Update getips.sh

--- getips.sh
+++ getips.sh
@@ -1,11 +1,11 @@
 ips=""
 ids=""
 while [ "$ids" = "" ]; do
-  ids=$(aws autoscaling describe-auto-scaling-groups --auto-scaling-group-names $ASG --region $REGION --query AutoScalingGroups[].Instances[].InstanceId --output text)
+  ids=$(aws autoscaling describe-auto-scaling-groups --auto-scaling-group-names $ASG --query AutoScalingGroups[].Instances[].InstanceId --output text)
   sleep 1
 done
 for ID in $ids;
 do
-    IP=$(aws ec2 describe-instances --instance-ids $ID --region $REGION --query Reservations[].Instances[].PrivateIpAddress --output text)
+    IP=$(aws ec2 describe-instances --instance-ids $ID --query Reservations[].Instances[].PrivateIpAddress --output text)
     ips="$ips,$IP"
 done

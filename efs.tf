3c28ca75afbb931f1735382743703529dbc8c0d9
gopac25 <39807096+gopac25@users.noreply.github.com>
2019-08-26 18:58:05 +0530

    Update efs.tf

--- efs.tf
+++ efs.tf
@@ -29,3 +29,19 @@ resource "aws_efs_file_system" "appdata_efs" {
    subnet_id = "${element(aws_subnet.moonshot-private-app.*.id, count.index)}"
    security_groups = ["${aws_security_group.allow-ssh.id}"]
  }
+
+resource "aws_efs_file_system" "dbdata_efs" {
+   creation_token = "dbdata_efs"
+   performance_mode = "generalPurpose"
+   throughput_mode = "bursting"
+   encrypted = "true"
+ tags = {
+     Name = "DB_Data_EFS"
+   }
+ }
+ 
+ resource "aws_efs_mount_target" "dbdata_efs_1" {
+   file_system_id  = "${aws_efs_file_system.dbdata_efs.id}"
+   subnet_id = "${element(aws_subnet.moonshot-private-dbmaster.*.id, count.index)}"
+   security_groups = ["${aws_security_group.allow-ssh.id}"]
+ }

1247bc18bdd6fc390c8a6abdc850084b3024c657
gopac25 <39807096+gopac25@users.noreply.github.com>
2019-08-26 13:27:00 +0530

    Update efs.tf

--- efs.tf
+++ efs.tf
@@ -9,7 +9,6 @@ resource "aws_efs_file_system" "webdata_efs" {
  }
  
  resource "aws_efs_mount_target" "webdata_efs_1" {
-    count = "${length(split(", ", lookup(var.azs, var.AWS_REGION)))}"
     file_system_id  = "${aws_efs_file_system.webdata_efs.id}"
     subnet_id = "${element(aws_subnet.moonshot-private-web.*.id, count.index)}"
     security_groups = ["${aws_security_group.allow-ssh.id}"]
@@ -26,7 +25,6 @@ resource "aws_efs_file_system" "appdata_efs" {
  }
  
  resource "aws_efs_mount_target" "appdata_efs_1" {
-   count = "${length(split(", ", lookup(var.azs, var.AWS_REGION)))}"
    file_system_id  = "${aws_efs_file_system.appdata_efs.id}"
    subnet_id = "${element(aws_subnet.moonshot-private-app.*.id, count.index)}"
    security_groups = ["${aws_security_group.allow-ssh.id}"]

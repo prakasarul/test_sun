8a75d4b5545e578c99e093cabe801cec3464dea1
gopac25 <39807096+gopac25@users.noreply.github.com>
2019-08-26 18:45:00 +0530

    Update elb.tf

--- elb.tf
+++ elb.tf
@@ -1,6 +1,6 @@
-resource "aws_elb" "my-elb" {
+resource "aws_elb" "my-elb-web" {
   name = "my-elb"
-  subnets = ["${aws_subnet.moonshot-private-web.id}", "${aws_subnet.moonshot-private-app.id}"]
+  subnets = ["${aws_subnet.moonshot-private-web.id}"]
   security_groups = ["${aws_security_group.elb-securitygroup.id}"]
  listener {
     instance_port = 80
@@ -20,6 +20,33 @@ resource "aws_elb" "my-elb" {
   connection_draining = true
   connection_draining_timeout = 400
   tags {
-    Name = "my-elb"
+    Name = "my-elb-web"
+  }
+}
+
+    
+resource "aws_elb" "my-elb-app" {
+  name = "my-elb"
+  subnets = ["${aws_subnet.moonshot-private-app.id}"]
+  security_groups = ["${aws_security_group.elb-securitygroup.id}"]
+ listener {
+    instance_port = 80
+    instance_protocol = "http"
+    lb_port = 80
+    lb_protocol = "http"
+  }
+  health_check {
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    timeout = 3
+    target = "HTTP:80/"
+    interval = 30
+  }
+
+  cross_zone_load_balancing = true
+  connection_draining = true
+  connection_draining_timeout = 400
+  tags {
+    Name = "my-elb-app"
   }
 }

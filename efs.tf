13092e7e4e90a1dfa9370ad388073c2ef1703b10
gopac25 <39807096+gopac25@users.noreply.github.com>
2019-08-26 18:43:13 +0530

    Update efs.tf

--- efs.tf
+++ efs.tf
@@ -29,19 +29,3 @@ resource "aws_efs_file_system" "appdata_efs" {
    subnet_id = "${element(aws_subnet.moonshot-private-app.*.id, count.index)}"
    security_groups = ["${aws_security_group.allow-ssh.id}"]
  }
-
-#data "template_file" "webm" {
- # template = "${file("webmount.sh.tpl")}"
-
-  #vars = {
-   # web = "${aws_efs_mount_target.webdata_efs_1.*.ip_address}"
-  #}
-#}
-
-#data "template_file" "appm" {
-#  template = "${file("appmount.sh.tpl")}"
-
- # vars = {
- #   app = "${aws_efs_mount_target.appdata_efs_1.*.ip_address}"
- # }
-#}

8f2e2c2cec72f922b482d55f8e22dc4d1e955410
gopac25 <39807096+gopac25@users.noreply.github.com>
2019-08-30 17:36:17 +0530

    Update inven.sh

--- inven.sh
+++ inven.sh
@@ -5,12 +5,12 @@ log=moviehub_inventory
 echo "# Ansible Inventory" >>$log
 echo "Version 1.0" >>$log
 echo "Author: Gopala Krishnan Chellappa <gopac25@gmail.com>" >>$log
-echo "\n\n" >>$log
+echo -e "\n\n" >>$log
 
 echo "###############################################################################" >>$log
 echo "# Groups" >>$log
 echo "###############################################################################" >>$log
-echo "\n" >>$log
+echo -e "\n" >>$log
 
 echo "[all-servers:children]" >>$log
 echo "jump" >>$log
@@ -18,23 +18,23 @@ echo "dbmaster" >>$log
 echo "dbslave" >>$log
 echo "web-servers" >>$log
 echo "app-servers" >>$log
-echo "\n" >>$log
+echo -e "\n" >>$log
 
 echo "[jump]" >>$log
 aws ec2 describe-instances --filters "Name=instance-state-name,Values=running" --query 'Reservations[].Instances[].[PrivateIpAddress,Tags[?Key==`Name`].Value[]]'| sed 's/None$/None\n/' | sed '$!N;s/\n/ /'|grep -i jump >>$log
-echo "\n" >>$log
+echo -e "\n" >>$log
 
 echo "[dbmaster]" >>$log
 aws ec2 describe-instances --filters "Name=instance-state-name,Values=running" --query 'Reservations[].Instances[].[PrivateIpAddress,Tags[?Key==`Name`].Value[]]'| sed 's/None$/None\n/' | sed '$!N;s/\n/ /'|grep -i master >>$log
-echo "\n" >>$log
+echo -e "\n" >>$log
 
 echo "[dbslave]" >>$log
 aws ec2 describe-instances --filters "Name=instance-state-name,Values=running" --query 'Reservations[].Instances[].[PrivateIpAddress,Tags[?Key==`Name`].Value[]]'| sed 's/None$/None\n/' | sed '$!N;s/\n/ /'|grep -i slave >>$log
-echo "\n" >>$log
+echo -e "\n" >>$log
 
 echo "[web-servers]" >>$log
 aws ec2 describe-instances --filters "Name=instance-state-name,Values=running" --query 'Reservations[].Instances[].[PrivateIpAddress,Tags[?Key==`Name`].Value[]]'| sed 's/None$/None\n/' | sed '$!N;s/\n/ /'|grep -i web >>$log
-echo "\n" >>$log
+echo -e "\n" >>$log
 
 echo "[app-servers]" >>$log
 aws ec2 describe-instances --filters "Name=instance-state-name,Values=running" --query 'Reservations[].Instances[].[PrivateIpAddress,Tags[?Key==`Name`].Value[]]'| sed 's/None$/None\n/' | sed '$!N;s/\n/ /'|grep -i app >>$log

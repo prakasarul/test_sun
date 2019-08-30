75fb497675269bcb61c58d8aebd37bf9513dff5a
gopac25 <39807096+gopac25@users.noreply.github.com>
2019-08-30 17:40:20 +0530

    Update inven.sh

--- inven.sh
+++ inven.sh
@@ -10,28 +10,25 @@ echo -e "\n" >>$log
 echo "###############################################################################" >>$log
 echo "# Groups" >>$log
 echo "###############################################################################" >>$log
-
+echo -e "\n" >>$log
 echo "[all-servers:children]" >>$log
 echo "jump" >>$log
 echo "dbmaster" >>$log
 echo "dbslave" >>$log
 echo "web-servers" >>$log
 echo "app-servers" >>$log
-
+echo -e "\n" >>$log
 echo "[jump]" >>$log
 aws ec2 describe-instances --filters "Name=instance-state-name,Values=running" --query 'Reservations[].Instances[].[PrivateIpAddress,Tags[?Key==`Name`].Value[]]'| sed 's/None$/None\n/' | sed '$!N;s/\n/ /'|grep -i jump >>$log
-
+echo -e "\n" >>$log
 echo "[dbmaster]" >>$log
 aws ec2 describe-instances --filters "Name=instance-state-name,Values=running" --query 'Reservations[].Instances[].[PrivateIpAddress,Tags[?Key==`Name`].Value[]]'| sed 's/None$/None\n/' | sed '$!N;s/\n/ /'|grep -i master >>$log
-
-
+echo -e "\n" >>$log
 echo "[dbslave]" >>$log
 aws ec2 describe-instances --filters "Name=instance-state-name,Values=running" --query 'Reservations[].Instances[].[PrivateIpAddress,Tags[?Key==`Name`].Value[]]'| sed 's/None$/None\n/' | sed '$!N;s/\n/ /'|grep -i slave >>$log
-
-
+echo -e "\n" >>$echo -e "\n" >>$log
 echo "[web-servers]" >>$log
 aws ec2 describe-instances --filters "Name=instance-state-name,Values=running" --query 'Reservations[].Instances[].[PrivateIpAddress,Tags[?Key==`Name`].Value[]]'| sed 's/None$/None\n/' | sed '$!N;s/\n/ /'|grep -i web >>$log
-
-
+echo -e "\n" >>$log
 echo "[app-servers]" >>$log
 aws ec2 describe-instances --filters "Name=instance-state-name,Values=running" --query 'Reservations[].Instances[].[PrivateIpAddress,Tags[?Key==`Name`].Value[]]'| sed 's/None$/None\n/' | sed '$!N;s/\n/ /'|grep -i app >>$log
